7c824a028a4110c4c30fe00ef3c32e9c91eec85f
Gayan Liyanagamage <gayanlggd@gmail.com>
2019-03-24 11:04:34 -0500

    parameterizing hardcoded values

--- ride_requests/__init__.py
+++ ride_requests/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 import mysql.connector
 
 app = Flask(__name__)
@@ -19,10 +19,33 @@ class RideReplay(Resource):
         return {'message':'get success'}, 200
 
     def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("VendorID")
+        parser.add_argument("tpep_pickup_datetime")
+        parser.add_argument("tpep_dropoff_datetime")
+        parser.add_argument("passenger_count")
+        parser.add_argument("trip_distance")
+        parser.add_argument("RatecodeID")
+        parser.add_argument("store_and_fwd_flag")
+        parser.add_argument("PULocationID")
+        parser.add_argument("DOLocationID")
+        parser.add_argument("payment_type")
+        parser.add_argument("fare_amount")
+        parser.add_argument("extra")
+        parser.add_argument("mta_tax")
+        parser.add_argument("tip_amount")
+        parser.add_argument("tolls_amount")
+        parser.add_argument("improvement_surcharge")
+        parser.add_argument("total_amount")
+
+        args = parser.parse_args()
+
         sql = "INSERT INTO yellow_tripdata_2018_12 (VendorID, tpep_pickup_datetime, tpep_dropoff_datetime , passenger_count, trip_distance, RatecodeID, "\
             "store_and_fwd_flag, PULocationID, DOLocationID, payment_type, fare_amount, extra, mta_tax, tip_amount, tolls_amount, "\
             "improvement_surcharge, total_amount) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-        val = ("1","2018-12-01 00:28:22","2018-12-01 00:44:07","2","2.50","1","N","148","234","1","12","0.5","0.5","3.95","0","0.3","17.25")
+        val = (args["VendorID"],args["tpep_pickup_datetime"],args["tpep_dropoff_datetime"],args["passenger_count"],args["trip_distance"],args["RatecodeID"], \
+            args["store_and_fwd_flag"],args["PULocationID"],args["DOLocationID"],args["payment_type"],args["fare_amount"],args["extra"],args["mta_tax"], \
+            args["tip_amount"],args["tolls_amount"],args["improvement_surcharge"],args["total_amount"])
         mycursor.execute(sql, val)
 
         mydb.commit()
